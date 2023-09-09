57af32824ab9575d1e435828ac94ab40b1181bd8
burakmey <burak_meydan-gs@hotmail.com>
2023-09-09 16:25:59 +0300

    Arrow added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Button from "./components/Button/Button";
+import Arrow from "./components/Arrow/Arrow";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import "./App.css";
 
 function App() {
@@ -6,9 +8,15 @@ function App() {
     <div className="App">
       App
       <div>
+        <Arrow className={"arrow-primary"}>
+          <IoIosArrowUp />
+        </Arrow>
         <Button name={"Play"} className={"main-button"} />
         <Button name={"Settings"} className={"sub-button"} />
         <Button name={"Rules"} className={"sub-button"} />
+        <Arrow className={"arrow-primary"}>
+          <IoIosArrowDown />
+        </Arrow>
       </div>
     </div>
   );
---
+++ src/components/Arrow/Arrow.js
@@ -0,0 +1,28 @@
+import { useState, memo } from "react";
+import "./Arrow.css";
+import "../styles.css";
+
+function Arrow(props) {
+  console.log("Arrow rendered!");
+
+  const [className, setClassName] = useState(`arrow ${props.className}`);
+
+  const updateStyle = (isOnMouse) => {
+    setClassName(
+      `arrow ${props.className} ${isOnMouse ? "arrow-on-mouse" : ""}`
+    );
+  };
+
+  return (
+    <div
+      className={className}
+      onMouseEnter={() => updateStyle(true)}
+      onMouseLeave={() => updateStyle(false)}
+      onClick={() => console.log("onClick")}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default memo(Arrow);
