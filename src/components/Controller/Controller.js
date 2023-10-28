7a2ca2b10db65890342254e8f77f6018bd3e01d2
burakmey <burak_meydan-gs@hotmail.com>
2023-10-28 17:57:41 +0300

    Controller edited

--- src/components/Controller/Controller.js
+++ src/components/Controller/Controller.js
@@ -1,30 +1,39 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Button from "../Button/Button";
 import "./Controller.css";
 
 function Controller() {
   console.log("Controller rendered!");
+
+  const [buttonName, setButtonName] = useState("Start");
+
   useEffect(() => {
     return () => console.log("Controller unmounted!");
   }, []);
 
-  const onClick = (buttonName) => {
+  useEffect(() => {}, [buttonName]);
+
+  const onClick = useCallback((buttonName) => {
     switch (buttonName) {
       case "--":
         break;
-      case "start":
+      case "Start":
+        setButtonName("Stop");
+        break;
+      case "Stop":
+        setButtonName("Start");
         break;
       case "++":
         break;
       default:
         break;
     }
-  };
+  }, []);
 
   return (
     <div className="controller">
       <Button name={"--"} className={"sub-button"} onClick={onClick} />
-      <Button name={"start"} className={"sub-button"} onClick={onClick} />
+      <Button name={buttonName} className={"sub-button"} onClick={onClick} />
       <Button name={"++"} className={"sub-button"} onClick={onClick} />
     </div>
   );
